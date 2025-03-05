d5e26a2a63266d08706cbdcdd5ed3f74c622331c
Claus Nielsen <clausn999@gmail.com>
2025-03-05 16:38:29 +0100

    Added ZonedDateTime <-> Date converters

---
+++ src/main/java/dk/clanie/mongo/convert/DateToZonedDateTimeConverter.java
@@ -0,0 +1,19 @@
+package dk.clanie.mongo.convert;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+
+@ReadingConverter
+public class DateToZonedDateTimeConverter implements Converter<Date, ZonedDateTime> {
+
+
+	@Override
+	public ZonedDateTime convert(Date source) {
+		return source.toInstant().atZone(ZoneOffset.UTC);
+	}
+
+
+}
---
+++ src/main/java/dk/clanie/mongo/convert/ZonedDateTimeToDateConverter.java
@@ -0,0 +1,18 @@
+package dk.clanie.mongo.convert;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.WritingConverter;
+
+@WritingConverter
+public class ZonedDateTimeToDateConverter implements Converter<ZonedDateTime, Date> {
+
+
+	@Override
+    public Date convert(ZonedDateTime source) {
+        return Date.from(source.toInstant());
+    }
+
+
+}
