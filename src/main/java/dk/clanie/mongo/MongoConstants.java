325b123c3adcb49ac9fccfd83afe53028cc7b1b0
Claus Nielsen <clausn999@gmail.com>
2025-11-28 12:31:23 +0100

    Fix copyright notices

--- src/main/java/dk/clanie/mongo/MongoConstants.java
+++ src/main/java/dk/clanie/mongo/MongoConstants.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2025, Claus Nielsen, clausn999@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package dk.clanie.mongo;
 
 import static dk.clanie.core.Utils.asUuid;
@@ -6,12 +23,10 @@ import java.util.UUID;
 
 public final class MongoConstants {
 
-
 	private MongoConstants() {
 		// Not meant to be instantiated
 	}
 
 	public static final UUID ADMIN_TENANT_ID = asUuid("00000000-0000-0000-0000-000000000000");
-
 
 }
--- src/main/java/dk/clanie/mongo/convert/ZonedDateTimeToDateConverter.java
+++ src/main/java/dk/clanie/mongo/convert/ZonedDateTimeToDateConverter.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2025, Claus Nielsen, clausn999@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package dk.clanie.mongo.convert;
 import java.time.ZonedDateTime;
 import java.util.Date;
@@ -8,11 +25,9 @@ import org.springframework.data.convert.WritingConverter;
 @WritingConverter
 public class ZonedDateTimeToDateConverter implements Converter<ZonedDateTime, Date> {
 
-
 	@Override
     public Date convert(ZonedDateTime source) {
         return Date.from(source.toInstant());
     }
-
 
 }
